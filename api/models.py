62cd4d440a7ad59d5915758c1a8f31dcc7b88bb1
CHAMOMILE <CHAMOM2LES@outlook.com>
2024-06-10 10:16:11 +0300

    add User

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class User(AbstractUser):
+    is_employee = models.BooleanField(default=False)
+    is_client = models.BooleanField(default=False)
+    phone = models.CharField(max_length=15, unique=True, null=True, blank=True)
 
 # Create your models here.
